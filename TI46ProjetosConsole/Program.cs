6c65dcecd262fa4c9f9124a6ccde7c01763b1253
VITORIA RAFAELA SILVA PRETEROTTO <vitoria.rspreterotto@senacsp.edu.br>
2024-09-30 20:16:33 -0300

    Primeira versão do projeto TI46Console

---
+++ TI46ProjetosConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI46ProjetosConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
