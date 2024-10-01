10d6b1ded71ed9e5b8659c8e2726559ef8d79a41
VITORIA RAFAELA SILVA PRETEROTTO <vitoria.rspreterotto@senacsp.edu.br>
2024-09-30 22:06:20 -0300

    v3 do programa, média do aluno

--- TI46ProjetosConsole/Program.cs
+++ TI46ProjetosConsole/Program.cs
@@ -10,8 +10,28 @@ namespace TI46ProjetosConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá, World!!!");
-            Console.WriteLine("Teste, linha dois");
+            string nome;
+            decimal n1, n2, n3, n4, res;
+
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Seja bem vindo, " + nome);
+
+            Console.WriteLine("Digite as notas: ");
+            n1 = decimal.Parse(Console.ReadLine());
+            n2 = decimal.Parse(Console.ReadLine());
+            n3 = decimal.Parse(Console.ReadLine());
+            n4 = decimal.Parse(Console.ReadLine());
+
+            res = ((n1 + n2 + n3 + n4)/4);
+
+            Console.WriteLine("O aluno " + nome + " teve média final de: " + res);
+
+
+            Console.ReadKey();
+
+
         }
     }
 }
